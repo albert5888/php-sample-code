edfb8fc93880e71160377ae2bec1e75200f51413
licheng_9207 <51775056+albert5888@users.noreply.github.com>
2022-04-22 12:41:09 +0800

    Create show_time.php

---
+++ show_time.php
@@ -0,0 +1,8 @@
+<?php
+date_default_timezone_set('Asia/Taipei');
+$dateYmd = date('Y-m-d');
+$datehms = date('h:i:s');
+echo "Current Year Month Day: $dateYmd";
+echo "\n";
+echo "Current Hour Minute Second: $datehms";
+?>
